d5854084faddff52310873effa50b89d99e2f2eb
Filip Mivalt <mivalt.filip@gmail.com>
2022-11-30 16:13:48 -0600

    Commit all files test

--- ncs2mef/test/__init__.py
+++ ncs2mef/test/__init__.py
@@ -3,15 +3,3 @@
 #
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
-
-
-from ncs2mef.ncs_reader import *
-
-__version__ = '0.0.1'
-
-
-
-
-
-
-
